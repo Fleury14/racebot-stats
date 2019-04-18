8dea2490a76d68059e710d1058b835448fa4a8cc
J.R. Ruggiero <jrruggiero@shift3tech.com>
2019-04-18 14:20:17 -0700

    feat(win loss): search form

--- src/components/win-loss/WinLoss.js
+++ src/components/win-loss/WinLoss.js
@@ -1,9 +1,12 @@
 import React, { Component } from 'react';
+import { Input, Button } from 'reactstrap';
 import { ParseWinLoss } from '../../helpers';
 
 class WinLoss extends Component {
   state = {
     parsedData: null,
+    playerInput: '',
+    foundNames: null,
   }
 
   componentDidMount() {
@@ -12,12 +15,31 @@ class WinLoss extends Component {
     this.setState({ parsedData: dataResult });
   }
 
+  onSubmitName() {
+    const { playerInput, parsedData } = this.state;
+    const foundNames = parsedData.find(race => race.name.toLowerCase() === playerInput.toLowerCase());
+    console.log('found', foundNames);
+  }
+
   render() {
     // console.log('props', this.props);
     console.log('state', this.state);
+    const { parsedData, playerInput } = this.state;
     return (
       <div>
         <h1>Win loss</h1>
+        {!parsedData && <p>There is no data to display</p>}
+        {parsedData && (
+          <div>
+            <p>{parsedData.length} players on record.</p>
+            <p>Input player name</p>
+            <div className="d-flex">
+              <Input className="mr-4" value={playerInput} onChange={e => this.setState({ playerInput: e.target.value })}/>
+              <Button color="primary" onClick={() => this.onSubmitName()}>Submit</Button>
+            </div>
+            
+          </div>
+        )}
       </div>
     );
   }
