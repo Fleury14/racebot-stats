0b420b9552fddd2b841a9d15de507d225695d536
J.R. Ruggiero <fleury14@comcast.net>
2019-04-20 15:54:44 -0700

    chore(redux): created get single race endpoint

--- src/redux/actions/BotActions.js
+++ src/redux/actions/BotActions.js
@@ -1,4 +1,4 @@
-import { DATA_START_LOADING, DATA_DONE_LOADING, BOT_DATA_ERROR, DATA_DONE_LOADING_RACER } from './types';
+import { DATA_START_LOADING, DATA_DONE_LOADING, BOT_DATA_ERROR, DATA_DONE_LOADING_RACER, DATA_DONE_LOADING_SINGLE_RACE } from './types';
 import axios from 'axios';
 
 const loadStart = () => ({
@@ -40,6 +40,19 @@ export const getRacerData = (racer) => {
       axios.get(`http://ec2-52-15-172-83.us-east-2.compute.amazonaws.com:8080/users?name=${racer}`, { headers: { apikey: process.env.REACT_APP_RACEBOT_APIKEY } })
       .then(response => {
         dispatch(loadFinish(response.data, DATA_DONE_LOADING_RACER));
+      })
+      .catch(err => {
+        dispatch(loadError(err));
+    });
+  }
+}
+
+export const getSingleRaceData = (key) => {
+  return (dispatch) => {
+    dispatch(loadStart());
+      axios.get(`http://ec2-52-15-172-83.us-east-2.compute.amazonaws.com:8080/races?key=${key}`, { headers: { apikey: process.env.REACT_APP_RACEBOT_APIKEY } })
+      .then(response => {
+        dispatch(loadFinish(response.data, DATA_DONE_LOADING_SINGLE_RACE));
       })
       .catch(err => {
         dispatch(loadError(err));
--- src/redux/actions/types.js
+++ src/redux/actions/types.js
@@ -1,5 +1,6 @@
 export const DATA_START_LOADING = 'data-loading';
 export const DATA_DONE_LOADING = 'data-done-loading';
 export const DATA_DONE_LOADING_RACER = 'data-done-loading-racer';
+export const DATA_DONE_LOADING_SINGLE_RACE = 'data-done-loading-single-race';
 export const BOT_DATA_ERROR = 'bot-data-error';
 export const GET_BOT_DATA = 'get-bot-data';
