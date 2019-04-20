d9f947c4c5e0827b0298f7357a5b1a7e4321ae78
J.R. Ruggiero <fleury14@comcast.net>
2019-04-20 16:06:58 -0700

    chore(selected race): data is brought in successfully

--- src/components/selected-race/SelectedRace.js
+++ src/components/selected-race/SelectedRace.js
@@ -1,12 +1,35 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { getSingleRaceData } from '../../redux/actions';
+
+const mapStateToProps = state => ({
+  raceData: state.botData.singleRaceData,
+});
+
+const mapActionsToProps = dispatch => ({
+  getRaceData(key) {
+    dispatch(getSingleRaceData(key))
+  }
+});
 
 class SelectedRace extends Component {
+  state ={
+    raceData: null,
+  }
+
+  componentDidMount() {
+    console.log('param', this.props.match.params.race);
+    this.props.getRaceData(this.props.match.params.race);
+    this.setState({ raceData: this.props.raceData });
+  }
+
   render() {
+    console.log('props', this.props);
+    console.log('state', this.state);
     return (
       <h1>Selected Race</h1>
     );
   }
 }
 
-export default connect(null, null)(SelectedRace);
+export default connect(mapStateToProps, mapActionsToProps)(SelectedRace);
