2a6ff60a07e66566db7c193232a9c975b5238f69
J.R. Ruggiero <jrruggiero@shift3tech.com>
2019-05-06 13:36:01 -0700

    feat(nav): sub nav list

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -6,6 +6,12 @@ const Navbar = (props) => {
     <div className="navbar-container open-sans">
       <h3>RACEBOT STATS</h3>
       <p>A stats page for the Discord Racebot built by Supremacy</p>
+      <div className="sub-nav">
+        <ul>
+          <li>Race Directory</li>
+          <li>Player Directory</li>
+        </ul>
+      </div>
     </div>
   );
 }
