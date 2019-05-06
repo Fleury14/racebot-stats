1cf52fa97ccff28812ff1825d682dadebb787dec
J.R. Ruggiero <jrruggiero@shift3tech.com>
2019-05-06 14:27:37 -0700

    feat(race directory): races in

--- src/components/race-directory/RaceDirectory.js
+++ src/components/race-directory/RaceDirectory.js
@@ -1,15 +1,64 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import RaceTable from './RaceTable';
 import { Navbar } from '..';
+import { getBotData } from '../../redux/actions';
+import './RaceDirectory.scss';
 
+const mapStateToProps = (state) => {
+  return {
+    botData: state.botData,
+  }
+}
+
+const mapActionsToProps = dispatch => ({
+  getData() {
+    dispatch(getBotData());
+  }
+});
 class RaceDirectory extends Component {
+  state = {
+    data: null,
+    error: null,
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.data !== this.state.data) {
+      this.setState({ data: prevState.data });
+    }
+  }
+
+  static getDerivedStateFromProps(nextProps, prevState) {
+    if (nextProps.botData.data !== prevState.data | nextProps.botData.error) {
+      return { data: nextProps.botData.data, error: nextProps.botData.error }
+    } else return null;
+  }
+
+  componentDidMount() {
+    this.props.getData();
+  }
+
+
   render() {
+    // console.log('props', this.props);
+    // console.log('state', this.state);
+    const { data } = this.state;
+    console.log(data.items);
+    const FERaces = data.items.filter(race => race.details && race.details.game && race.details.game === 'ff4fe');
+    FERaces.sort((a, b) => {
+      const timeA = new Date(a.details.created);
+      const timeB = new Date(b.details.created);
+      return timeB.getTime() - timeA.getTime();
+    })
     return (
       <div>
         <Navbar />
-        <h1>Race Directory</h1>
+        <div className="race-directory-body">
+          <RaceTable data={FERaces} />
+        </div>
       </div>
     );
   }
 }
 
-export default RaceDirectory;
+export default connect(mapStateToProps, mapActionsToProps)(RaceDirectory);
---
+++ src/components/race-directory/RaceTable.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import { Table } from 'reactstrap';
+import { Link } from 'react-router-dom';
+import './CurrentRaces.scss';
+
+const renderRaceRow = (race, index) => {
+  const startDate = new Date(race.details.created);
+  if (index < 50) return (
+    <tr key={race.key}>
+      <th>
+        <Link to={`/race/${race.key}`}>{race.key}</Link>
+      </th>
+      <td>{race.details.guild.name}</td>
+      <td>{race.details.async ? 'Yes' : 'No'}</td>
+      <td>{race.details.entrants.length}</td>
+      <td>{startDate.toLocaleString()}</td>
+      <td>{race.details.mode}</td>
+    </tr>
+  );
+}
+
+const CurrentRaces = (props) => {
+  const { data } = props;
+  return (
+    <div className="current-races-container open-sans">
+      <h1 className="text-center text-uppercase">Current Races</h1>
+      {/* <p>Number of races running: { data.length }</p> */}
+      <Table>
+        <thead>
+          <tr>
+            <th>Tag</th>
+            <th>Location</th>
+            <th>Async?</th>
+            <th>Entrants</th>
+            <th>Time Started</th>
+            <th>Mode</th>
+          </tr>
+        </thead>
+        <tbody>
+          {data.map((race, index) => renderRaceRow(race, index))}
+        </tbody>
+      </Table>
+    </div>
+  );
+};
+
+export default CurrentRaces;
