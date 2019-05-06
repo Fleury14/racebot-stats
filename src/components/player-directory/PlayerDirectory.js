4fe249f3896d4005b95d442f3aff394c059ed0cf
J.R. Ruggiero <jrruggiero@shift3tech.com>
2019-05-06 15:29:31 -0700

    chore(various): wired up player directory

---
+++ src/components/player-directory/PlayerDirectory.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './PlayerDirectory.scss';
+
+class PlayerDirectory extends Component {
+  render() {
+    return (
+      <h1>Player Directory</h1>
+    );
+  }
+}
+
+export default PlayerDirectory;
--- src/router/AppRouter.js
+++ src/router/AppRouter.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
-import { Main, RacerStats, SelectedRace, RaceDirectory } from '../components'
+import { Main, RacerStats, SelectedRace, RaceDirectory, PlayerDirectory } from '../components'
 // import Main from '../components/main/Main';
 
 class AppRouter extends Component {
@@ -11,6 +11,7 @@ class AppRouter extends Component {
         <Route path="/racer/:racer" exact component={RacerStats} />
         <Route path="/race/:race" exact component={SelectedRace} />
         <Route path="/race-directory" exact component={RaceDirectory} />
+        <Route path="/player-directory" exact component={PlayerDirectory} />
       </Router>
     );
   }
