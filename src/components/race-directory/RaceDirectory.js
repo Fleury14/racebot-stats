8f6a830bf6d83b4378321b537e68ecabc57f406c
J.R. Ruggiero <jrruggiero@shift3tech.com>
2019-05-06 13:45:41 -0700

    chore(race director): added navbar

--- src/components/race-directory/RaceDirectory.js
+++ src/components/race-directory/RaceDirectory.js
@@ -1,9 +1,13 @@
 import React, { Component } from 'react';
+import { Navbar } from '..';
 
 class RaceDirectory extends Component {
   render() {
     return (
-      <h1>Race Directory</h1>
+      <div>
+        <Navbar />
+        <h1>Race Directory</h1>
+      </div>
     );
   }
 }
