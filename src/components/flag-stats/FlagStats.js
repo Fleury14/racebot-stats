6ffe4c8a3ed33acb2e22d2c2db1db58533a47ab2
J.R. Ruggiero <jrruggiero@shift3tech.com>
2019-05-15 13:42:13 -0700

    feat(flagstats): c, chars

--- src/components/flag-stats/FlagStats.js
+++ src/components/flag-stats/FlagStats.js
@@ -53,7 +53,7 @@ class FlagStats extends Component {
           <p className="text-center mb-5">Races recorded: {data.total}</p>
             <Container fluid>
               <Row>
-                <Col md="4">
+                <Col md="4" className="mb-4">
                   <div className="text-center">
                     <h2>V Flag</h2>
                     <p>None: {data.V[0]} ({(data.V[0] / data.total * 100).toFixed(1)}%)</p>
@@ -61,7 +61,7 @@ class FlagStats extends Component {
                     <p>V2: {data.V[2]} ({(data.V[2] / data.total * 100).toFixed(1)}%)</p>
                   </div>
                 </Col>
-                <Col md="4">
+                <Col md="4" className="mb-4">
                   <div className="text-center">
                     <h2>J Flag</h2>
                     <p>None: {data.J[0]} ({(data.J[0] / data.total * 100).toFixed(1)}%)</p>
@@ -70,7 +70,7 @@ class FlagStats extends Component {
                     <p>Ja: {data.J.a} ({(data.J.a / data.total * 100).toFixed(1)}%)</p>
                   </div>
                 </Col>
-                <Col md="4">
+                <Col md="4" className="mb-4">
                   <div className="text-center">
                     <h2>K Flag</h2>
                     <p>None: {data.K[0]} ({(data.K[0] / data.total * 100).toFixed(1)}%)</p>
@@ -81,7 +81,7 @@ class FlagStats extends Component {
                     <p>K!: {data.K.noSafety} ({(data.K.noSafety / data.total * 100).toFixed(1)}%)</p>
                   </div>
                 </Col>
-                <Col md="4">
+                <Col md="4" className="mb-4">
                   <div className="text-center">
                     <h2>C Flag</h2>
                     <p>None: {data.C[0]} ({(data.C[0] / data.total * 100).toFixed(1)}%)</p>
@@ -92,6 +92,76 @@ class FlagStats extends Component {
                     <p>-no-dupes: {data.other.noDupes} ({(data.other.noDupes / data.total * 100).toFixed(1)}%)</p>
                     <p>-rescue: {data.other.rescue} ({(data.other.rescue / data.total * 100).toFixed(1)}%)</p>
                     <p>-hobs: {data.other.hobs} ({(data.other.hobs / data.total * 100).toFixed(1)}%)</p>
+                  </div>
+                </Col>
+                <Col md="8" className="mb-4">
+                  <div className="text-center">
+                    <h2>Character restrictions</h2>
+                    <div className="d-flex flag-stats-char">
+                        <h3 className="w-33">Char</h3>
+                        <h3 className="w-33">Start</h3>
+                        <h3 className="w-33">Remove</h3>
+                    </div>
+                    <div className="d-flex flag-stats-char">
+                        <p className="w-33">Cecil</p>
+                        <p className="w-33">{data.charStart.cecil}</p>
+                        <p className="w-33">{data.charRestrict.cecil}</p>
+                    </div>
+                    <div className="d-flex flag-stats-char">
+                        <p className="w-33">Kain</p>
+                        <p className="w-33">{data.charStart.kain}</p>
+                        <p className="w-33">{data.charRestrict.kain}</p>
+                    </div>
+                    <div className="d-flex flag-stats-char">
+                        <p className="w-33">Rydia</p>
+                        <p className="w-33">{data.charStart.rydia}</p>
+                        <p className="w-33">{data.charRestrict.rydia}</p>
+                    </div>
+                    <div className="d-flex flag-stats-char">
+                        <p className="w-33">Tellah</p>
+                        <p className="w-33">{data.charStart.tellah}</p>
+                        <p className="w-33">{data.charRestrict.tellah}</p>
+                    </div>
+                    <div className="d-flex flag-stats-char">
+                        <p className="w-33">Edward</p>
+                        <p className="w-33">{data.charStart.edward}</p>
+                        <p className="w-33">{data.charRestrict.edward}</p>
+                    </div>
+                    <div className="d-flex flag-stats-char">
+                        <p className="w-33">Rosa</p>
+                        <p className="w-33">{data.charStart.rosa}</p>
+                        <p className="w-33">{data.charRestrict.rosa}</p>
+                    </div>
+                    <div className="d-flex flag-stats-char">
+                        <p className="w-33">Yang</p>
+                        <p className="w-33">{data.charStart.yang}</p>
+                        <p className="w-33">{data.charRestrict.yang}</p>
+                    </div>
+                    <div className="d-flex flag-stats-char">
+                        <p className="w-33">Palom</p>
+                        <p className="w-33">{data.charStart.palom}</p>
+                        <p className="w-33">{data.charRestrict.palom}</p>
+                    </div>
+                    <div className="d-flex flag-stats-char">
+                        <p className="w-33">Porom</p>
+                        <p className="w-33">{data.charStart.porom}</p>
+                        <p className="w-33">{data.charRestrict.porom}</p>
+                    </div>
+                    <div className="d-flex flag-stats-char">
+                        <p className="w-33">Cid</p>
+                        <p className="w-33">{data.charStart.cid}</p>
+                        <p className="w-33">{data.charRestrict.cid}</p>
+                    </div>
+                    <div className="d-flex flag-stats-char">
+                        <p className="w-33">Edge</p>
+                        <p className="w-33">{data.charStart.edge}</p>
+                        <p className="w-33">{data.charRestrict.edge}</p>
+                    </div>
+                    <div className="d-flex flag-stats-char">
+                        <p className="w-33">FuSoYa</p>
+                        <p className="w-33">{data.charStart.fusoya}</p>
+                        <p className="w-33">{data.charRestrict.fusoya}</p>
+                    </div>
                   </div>
                 </Col>
               </Row>
