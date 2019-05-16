cdd2c3cdf57685f80307ee4f8dc35b1a7f3f8e91
J.R. Ruggiero <jrruggiero@shift3tech.com>
2019-05-16 13:03:37 -0700

    fix(selected race): use entrants instead of finishers

--- src/components/twov2-stats/Twov2Stats.js
+++ src/components/twov2-stats/Twov2Stats.js
@@ -5,9 +5,11 @@ import './Twov2Stats.scss';
 
 const Twov2Stats = (props) => {
   const { data, currentRacer } = props;
+  console.log('data', data)
   return (
     <div className="pairs-container">
       <h1 className="text-center text-uppercase">Partner Stats (2v2)</h1>
+      <h4 className="text-center text-uppercase">(Races entered) Record</h4>
       {data.map((team, index) => {
         return (
           <div key={index} className="d-flex">
@@ -15,7 +17,7 @@ const Twov2Stats = (props) => {
               <h3>{team.racer1Name === currentRacer ? team.racer2Name : team.racer1Name}:</h3>
             </div>
             <div className="w-50 pr-3 text-left">
-              <h4>{team.wins} - {team.losses}</h4>
+              <h4>({team.races_entered.length}) {team.wins} - {team.losses}</h4>
             </div>
           </div>
         );
