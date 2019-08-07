6fcb496df0885c894392d2499af85b316bec978d
J.R. Ruggiero <jrruggiero@shift3tech.com>
2019-08-07 10:24:49 -0700

    chore(parse wagers): race looping

--- src/helpers/ParseWagers.js
+++ src/helpers/ParseWagers.js
@@ -1,5 +1,17 @@
 const ParseWagers = (races) => {
   console.log('races', races);
+  races.forEach(race => {
+    if (race.key === 'ff4fe-qcnnd') {
+      console.log('race', race);
+    }
+    // if (race.details && race.details.entrants) {
+    //   race.details.entrants.forEach(entrant => {
+    //     if (entrant.wager) {
+    //       console.log('wager?', race);
+    //     }
+    //   })
+    // }
+  });
   return;
 }
 
