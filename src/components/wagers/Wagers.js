01a93a1b2d7309fbb2da7fa52e399256f87c4bd2
J.R. Ruggiero <jrruggiero@shift3tech.com>
2019-08-15 11:27:11 -0700

    refac(wagers): use facc

--- src/components/wagers/Wagers.js
+++ src/components/wagers/Wagers.js
@@ -1,21 +1,7 @@
 import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import { getBotData } from '../../redux/actions';
 import { Navbar } from '..';
+import { ReduxMainData } from '../redux-data';
 import { ParseWagers } from '../../helpers';
-
-const mapStateToProps = (state) => {
-  return {
-    botData: state.botData,
-    loading: state.botData.loading,
-  }
-}
-
-const mapActionsToProps = dispatch => ({
-  getData() {
-    dispatch(getBotData());
-  }
-});
 
 class Wagers extends Component {
   state = {
@@ -23,34 +9,19 @@ class Wagers extends Component {
     error: null,
     loading: false
   }
-
-  componentDidUpdate(prevProps, prevState) {
-    if (prevState.data !== this.state.data) {
-      this.setState({ data: prevState.data });
-    }
-  }
-
-  static getDerivedStateFromProps(nextProps, prevState) {
-    if (nextProps.botData.data !== prevState.data | nextProps.botData.error) {
-      return { data: nextProps.botData.data, error: nextProps.botData.error, loading: nextProps.botData.loading }
-    } else if(nextProps.loading !== prevState.loading) {
-      return { loading: nextProps.loading };
-    } else {
-      return null;
-    }
-  }
-  
-  componentDidMount() {
-    this.props.getData();
-  }
   
   render() {
-    const wageData = ParseWagers(this.state.data);
     // console.log('proppys', this.props);
     // console.log('stizzate', this.state);
     return (
       <div>
         <Navbar />
+        <ReduxMainData>
+          {(reduxData) => {
+            const wageData = ParseWagers(reduxData.botData);
+            console.log('wageData', wageData);
+          }}
+        </ReduxMainData>
         <h1>Wagers</h1>
       </div>
     );
@@ -58,4 +29,4 @@ class Wagers extends Component {
   
 }
 
-export default connect(mapStateToProps, mapActionsToProps)(Wagers)
+export default Wagers;
