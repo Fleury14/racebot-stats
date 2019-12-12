a25f75c64035af91f36ded2c4f51d9459fb289b3
J.R. Ruggiero <jrruggiero@shift3tech.com>
2019-12-12 11:03:58 -0800

    feat(navbar): added featured

--- src/components/featured/Featured.js
+++ src/components/featured/Featured.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import { Navbar } from '..';
 import './Featured.scss';
 
 const Featured = (props) => {
   return (
     <div>
+      <Navbar />
       <h1>Featured</h1>
     </div>
   );
