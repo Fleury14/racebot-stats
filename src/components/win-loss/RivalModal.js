bd6e884d87563874883701589249d583cdd8cfb2
J.R. Ruggiero <jrruggiero@shift3tech.com>
2020-01-29 16:54:39 -0800

    refac(rival modal): paginator complete

--- src/components/win-loss/RivalModal.js
+++ src/components/win-loss/RivalModal.js
@@ -4,16 +4,32 @@ import { Link } from 'react-router-dom';
 import './RivalModal.scss';
 
 class RivalModal extends Component {
+  state = {
+    paginate: this.props.raceData && this.props.raceData.length > 30,
+    page: 1,
+    pageLength: 30,
+  };
+
   render() {
     const { close, player, rival, raceData } = this.props;
+    const { paginate, page, pageLength } = this.state;
     if (!raceData) return null;
     raceData.sort((a, b) => {
       const aDate = new Date(a.details.created).getTime();
       const bDate = new Date(b.details.created).getTime();
       return bDate - aDate;
-    })
+    });
+    const displayedRaces = paginate ? raceData.slice((page - 1) * pageLength, page * pageLength) : raceData;
     const playerName = raceData[0].details.entrants.find(entrant => entrant.id === player).name;
     const rivalName = raceData[0].details.entrants.find(entrant => entrant.id === rival).name;
+
+    // handle pagination
+    const pageNumbers = [];
+    for (let i = 0; i < Math.ceil(raceData.length / pageLength); i++) {
+      pageNumbers.push(<span key={`page-${i+1}`} className={`page-number ${i + 1 === page ? 'page-active' : ''}`} onClick={() => this.setState({ page: i + 1 })}>{i + 1}</span>)
+    }
+
+
     return (
       <div className="rival-modal-bg open-sans">
         <div className="rival-modal-body">
@@ -33,7 +49,7 @@ class RivalModal extends Component {
                 <h4>{rivalName}</h4>
               </Col>
             </Row>
-            {raceData.map(race => {
+            {displayedRaces.map(race => {
               const playerData = race.details.entrants.find(entrant => entrant.id === player);
               const rivalData = race.details.entrants.find(entrant => entrant.id === rival)
               const raceDate = new Date(race.details.created);
@@ -61,6 +77,10 @@ class RivalModal extends Component {
               )
             })}
           </Container>
+          {paginate ? <div className="paginator">
+            <span>Page</span>
+            {pageNumbers}
+          </div> : null}
           <div className="d-flex justify-content-center">
             <Button color="danger" onClick={() => close()}>Close</Button>
           </div>
