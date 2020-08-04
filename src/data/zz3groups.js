b5914d303fb25ad4390a4a878475a58f736ddf83
J.R. Ruggiero <jrruggiero@shift3tech.com>
2020-08-03 17:00:55 -0700

    chore(data): 3z group names

---
+++ src/data/zz3groups.js
@@ -0,0 +1,114 @@
+const zz3Groups = [{
+  group: "alexander",
+  members: []
+},
+{
+  group: "anima",
+  members: []
+},
+{
+  group: "bahamut",
+  members: []
+},
+{
+  group: "carbuncle",
+  members: []
+},
+{
+  group: "chocobo",
+  members: []
+},
+{
+  group: "doomtrain",
+  members: []
+},
+{
+  group: "gilgamesh",
+  members: []
+},
+{
+  group: "golem",
+  members: []
+},
+{
+  group: "ifrit",
+  members: []
+},
+{
+  group: "lakshmi",
+  members: []
+},
+{
+  group: "leviathan",
+  members: []
+},
+{
+  group: "magus-sisters",
+  members: []
+},
+{
+  group: "mist-dragon",
+  members: []
+},
+{
+  group: "odin",
+  members: []
+},
+{
+  group: "phoenix",
+  members: []
+},
+{
+  group: "ramuh",
+  members: []
+},
+{
+  group: "ravana",
+  members: []
+},
+{
+  group: "shiva",
+  members: []
+},
+{
+  group: "shoat",
+  members: []
+},
+{
+  group: "siren",
+  members: []
+},
+{
+  group: "syldra",
+  members: []
+},
+{
+  group: "sylph",
+  members: []
+},
+{
+  group: "titan",
+  members: []
+},
+{
+  group: "tonberry",
+  members: []
+},
+{
+  group: "tritoch",
+  members: []
+},
+{
+  group: "ultima",
+  members: []
+},
+{
+  group: "valefor",
+  members: []
+},
+{
+  group: "zodiark",
+  members: []
+}];
+
+export default zz3Groups;
