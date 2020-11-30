a9f719de9916fedcbbc2389cd32afd2c4fb01c21
J.R. Ruggiero <jrruggiero@shift3tech.com>
2020-11-30 13:53:32 -0800

    chore(club board): convert to class based, lay out expectations

--- src/components/club-leaderboard/ClubLeaderboard.js
+++ src/components/club-leaderboard/ClubLeaderboard.js
@@ -1,24 +1,32 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './ClubLeaderboard.scss';
 import { ReduxEventsData } from '../redux-data';
 
 const CLUB_NAMES = ['Underground Racing Club Season 2', 'Lunar Racing Club Season 2', 'MJC 2', 'Enterprise Legacy', 'SS2']
 
-const ClubLeaderboard = (props) => {
-  return (
-    <ReduxEventsData>
-      {(eventData) => {
-        const relevantEvents = eventData && eventData.events && eventData.events.items.length ? eventData.events.items.filter(event => event.name && CLUB_NAMES.indexOf(event.name) >= 0) : [];
-        console.log('relevent data', relevantEvents);
-        return (
-          <div className="club-leaderboard-container">
-            <h2>Club Leaderboard</h2>
-          </div>
-        )
-      }}
-    </ReduxEventsData>
-    
-  );
+class ClubLeaderboard extends Component {
+  state = {
+    activeClub: 0,
+  };
+
+  render() {
+    return (
+      <ReduxEventsData>
+        {(eventData) => {
+          const relevantEvents = eventData && eventData.events && eventData.events.items.length ? eventData.events.items.filter(event => event.name && CLUB_NAMES.indexOf(event.name) >= 0) : [];
+          console.log('relevent data', relevantEvents);
+          return (
+            <div className="club-leaderboard-container">
+              <h2>Club Leaderboard</h2>
+              {/* Club header that can select onr of the clubs and adjusts state when selected */}
+              {/* Club content that displays the data of the selected index, for now just name and rating */}
+            </div>
+          )
+        }}
+      </ReduxEventsData>
+      
+    );
+  } 
 }
 
 export default ClubLeaderboard;
