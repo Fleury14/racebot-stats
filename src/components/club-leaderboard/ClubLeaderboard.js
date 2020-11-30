bb91edc9401593d519915ae357b782149a854cec
J.R. Ruggiero <jrruggiero@shift3tech.com>
2020-11-30 13:42:58 -0800

    chore(club board): bring in event data and filter out irrelevant clubs

--- src/components/club-leaderboard/ClubLeaderboard.js
+++ src/components/club-leaderboard/ClubLeaderboard.js
@@ -1,11 +1,23 @@
 import React from 'react';
 import './ClubLeaderboard.scss';
+import { ReduxEventsData } from '../redux-data';
+
+const CLUB_NAMES = ['Underground Racing Club Season 2', 'Lunar Racing Club Season 2', 'MJC 2', 'Enterprise Legacy', 'SS2']
 
 const ClubLeaderboard = (props) => {
   return (
-    <div class="club-leaderboard-container">
-      <h2>Club Board</h2>
-    </div>
+    <ReduxEventsData>
+      {(eventData) => {
+        const relevantEvents = eventData && eventData.events && eventData.events.items.length ? eventData.events.items.filter(event => event.name && CLUB_NAMES.indexOf(event.name) >= 0) : [];
+        console.log('relevent data', relevantEvents);
+        return (
+          <div className="club-leaderboard-container">
+            <h2>Club Leaderboard</h2>
+          </div>
+        )
+      }}
+    </ReduxEventsData>
+    
   );
 }
 
