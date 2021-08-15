9e271eaf498dfbd35a4957b96aed13bba57140c1
J.R. Ruggiero <jrruggiero@shift3tech.com>
2021-08-15 13:13:20 -0700

    chore(navbar and zz4): update zz4 race, add link to navbar

--- src/data/zz4QualList.js
+++ src/data/zz4QualList.js
@@ -1,15 +1,5 @@
 export const qualifiers = [
-  'ff4fe-fjie3',
-  'ff4fe-5mevw',
-  'ff4fe-cybxc',
-  'ff4fe-ur8i1',
-  'ff4fe-txmuw',
-  'ff4fe-umy0e',
-  'ff4fe-xxwgp',
-  'ff4fe-grey1',
-  'ff4fe-s3aze',
-  'ff4fe-dx38w',
-  'ff4fe-yxbpi',
-
+  'ff4fe-wvvz7o',
+  
   
 ];
