59fd3802338a3ae9c8f2caa882334a4fb37844c4
J.R. Ruggiero <jrruggiero@shift3tech.com>
2021-08-16 20:47:20 -0700

    fix(parsezz4): update qual list, fix bug with forfeited racers having undefined placement

--- src/helpers/ParzeZZ4.js
+++ src/helpers/ParzeZZ4.js
@@ -9,7 +9,7 @@ const parseZZ4 = (data) => {
 
   // filter out races that aren't in the qualifier array
   const qualifierRaces = data.items.filter(race => qualifiers.indexOf(race.key) >= 0);
-  console.log('qualifier races', qualifierRaces);
+  
 
   const zz4Entrants = [];
 
@@ -49,8 +49,11 @@ const parseZZ4 = (data) => {
     });
 
     // figure out the average from the top 6 remaining
-    filteredEntrants.sort((a, b) => a.placement - b.placement);
-    
+    filteredEntrants.sort((a, b) => {
+      const place1 = a.placement !== undefined ? a.placement : 999;
+      const place2 = b.placement !== undefined ? b.placement : 999;
+      return place1 - place2;
+    });
     const top6 = filteredEntrants.filter((val, index) => index <= 5);
     // console.log('race', race);
     const startTime = new Date(race.startTime).getTime();
