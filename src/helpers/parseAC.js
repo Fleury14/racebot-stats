c7b1577281504788d158c32c0ef6905d7ddb224c
J.R. Ruggiero <jrruggiero@shift3tech.com>
2022-03-06 22:36:27 -0800

    chore(package/parser): install and uninstall tabletop

--- src/helpers/parseAC.js
+++ src/helpers/parseAC.js
@@ -1,5 +1,11 @@
+
 const parseAC = () => {
   console.log('parsing ac');
+
+  const authkey = process.env.REACT_APP_GOOGLE_AUTH_KEY;
+  const publicSpreadsheetUrl = 'https://docs.google.com/spreadsheets/d/1uMig-7JbT_L6kLRo1cRnwOzSpLWlIUbqAEQlwZUTI88/edit?usp=sharing';
+
+  
 }
 
 export default parseAC;
