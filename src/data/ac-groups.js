c1f8ed2a2cdf94712f28f3e941eca09967839676
J.R. Ruggiero <jrruggiero@shift3tech.com>
2022-03-07 13:22:48 -0800

    chore(data): add static groups in case of no API

---
+++ src/data/ac-groups.js
@@ -0,0 +1,645 @@
+const groups = [
+  {
+    title: "Artemis Bow",
+    players: [
+      {
+        name: 'David B',
+        wins: 0,
+        losses: 0,
+      },
+      {
+        name: 'JBrunTR',
+        wins: 0,
+        losses: 0,
+      },
+      {
+        name: 'Mechalink',
+        wins: 0,
+        losses: 0,
+      },
+      {
+        name: 'John Brickhead',
+        wins: 0,
+        losses: 0,
+      },
+      {
+        name: 'frankiebones',
+        wins: 0,
+        losses: 0,
+      },
+      {
+        name: 'PlumeriaKnight',
+        wins: 0,
+        losses: 0,
+      },
+      {
+        name: 'Blue Cat Loach',
+        wins: 0,
+        losses: 0,
+      },
+    ]
+  },
+  {
+    title: "Blitz Whip",
+    players: [
+      {
+        name: 'Y2Sky',
+        wins: 0,
+        losses: 0,
+      },
+      {
+        name: 'dnarii',
+        wins: 0,
+        losses: 0,
+      },
+      {
+        name: 'recklesscharlie',
+        wins: 0,
+        losses: 0,
+      },
+      {
+        name: 'CubsRule21',
+        wins: 0,
+        losses: 0,
+      },
+      {
+        name: 'soapboxgamer',
+        wins: 0,
+        losses: 0,
+      },
+      {
+        name: 'Sorbius',
+        wins: 0,
+        losses: 0,
+      },
+    ]
+  },
+  {
+    title: "Cat Claw",
+    players: [
+      {
+        name: 'JudgeJoe',
+        wins: 0,
+        losses: 0,
+      },
+      {
+        name: 'Keddril',
+        wins: 0,
+        losses: 0,
+      },
+      {
+        name: 'Poidrac',
+        wins: 0,
+        losses: 0,
+      },
+      {
+        name: 'EizanTayama',
+        wins: 0,
+        losses: 0,
+      },
+      {
+        name: 'LordBobBree',
+        wins: 0,
+        losses: 0,
+      },
+      {
+        name: 'scratchdragon',
+        wins: 0,
+        losses: 0,
+      },
+    ]
+  },
+  {
+    title: "Defense Sword",
+    players: [
+      {
+        name: 'McBain',
+        wins: 0,
+        losses: 0,
+      },
+      {
+        name: 'Dragus',
+        wins: 0,
+        losses: 0,
+      },
+      {
+        name: 'nitzy',
+        wins: 0,
+        losses: 0,
+      },
+      {
+        name: 'Gambit017',
+        wins: 0,
+        losses: 0,
+      },
+      {
+        name: 'The Bardic Panda',
+        wins: 0,
+        losses: 0,
+      },
+      {
+        name: 'Zeke Highwind',
+        wins: 0,
+        losses: 0,
+      },
+    ]
+  },
+  {
+    title: "Full Moon",
+    players: [
+      {
+        name: 'Funnelcakes',
+        wins: 0,
+        losses: 0,
+      },
+      {
+        name: 'Starman',
+        wins: 0,
+        losses: 0,
+      },
+      {
+        name: 'Fleury14',
+        wins: 0,
+        losses: 0,
+      },
+      {
+        name: 'AsmadiGames',
+        wins: 0,
+        losses: 0,
+      },
+      {
+        name: 'Professor Renderer',
+        wins: 0,
+        losses: 0,
+      },
+      {
+        name: 'Possumorpheus',
+        wins: 0,
+        losses: 0,
+      },
+    ]
+  },
+  {
+    title: "Lilith Rod",
+    players: [
+      {
+        name: 'Eykir',
+        wins: 0,
+        losses: 0,
+      },
+      {
+        name: 'Lenophis',
+        wins: 0,
+        losses: 0,
+      },
+      {
+        name: 'Pyre',
+        wins: 0,
+        losses: 0,
+      },
+      {
+        name: 'Infinious',
+        wins: 0,
+        losses: 0,
+      },
+      {
+        name: 'BadKarma',
+        wins: 0,
+        losses: 0,
+      },
+      {
+        name: 'Friedghoughphtheightteau',
+        wins: 0,
+        losses: 0,
+      },
+    ]
+  },
+  {
+    title: "Ogre Axe",
+    players: [
+      {
+        name: 'Zilch',
+        wins: 0,
+        losses: 0,
+      },
+      {
+        name: 'Marten Broadcloak',
+        wins: 0,
+        losses: 0,
+      },
+      {
+        name: 'Slippery',
+        wins: 0,
+        losses: 0,
+      },
+      {
+        name: 'GeminiHero',
+        wins: 0,
+        losses: 0,
+      },
+      {
+        name: 'Soleras',
+        wins: 0,
+        losses: 0,
+      },
+      {
+        name: 'wewbear',
+        wins: 0,
+        losses: 0,
+      },
+    ]
+  },
+  {
+    title: "Spear Spear",
+    players: [
+      {
+        name: 'Dia',
+        wins: 0,
+        losses: 0,
+      },
+      {
+        name: 'Allisele',
+        wins: 0,
+        losses: 0,
+      },
+      {
+        name: 'Rowdy Roddy Sniper',
+        wins: 0,
+        losses: 0,
+      },
+      {
+        name: 'Flyeaglesfly72',
+        wins: 0,
+        losses: 0,
+      },
+      {
+        name: 'Engidave',
+        wins: 0,
+        losses: 0,
+      },
+      {
+        name: 'EscoNitz',
+        wins: 0,
+        losses: 0,
+      },
+    ]
+  },
+  {
+    title: "Aegis Shield",
+    players: [
+      {
+        name: 'Xyrak',
+        wins: 0,
+        losses: 0,
+      },
+      {
+        name: 'BeautyInDiscovery',
+        wins: 0,
+        losses: 0,
+      },
+      {
+        name: 'Vitasia',
+        wins: 0,
+        losses: 0,
+      },
+      {
+        name: 'elvensorrow',
+        wins: 0,
+        losses: 0,
+      },
+      {
+        name: 'tallgrant',
+        wins: 0,
+        losses: 0,
+      },
+      {
+        name: 'Nightdew',
+        wins: 0,
+        losses: 0,
+      },
+    ]
+  },
+  {
+    title: "Crystal Ring",
+    players: [
+      {
+        name: 'Tybalt',
+        wins: 0,
+        losses: 0,
+      },
+      {
+        name: 'Xenocat',
+        wins: 0,
+        losses: 0,
+      },
+      {
+        name: 'Saradin',
+        wins: 0,
+        losses: 0,
+      },
+      {
+        name: 'sylverfyre',
+        wins: 0,
+        losses: 0,
+      },
+      {
+        name: 'Alchemie',
+        wins: 0,
+        losses: 0,
+      },
+      {
+        name: 'Rexx Raul',
+        wins: 0,
+        losses: 0,
+      },
+    ]
+  },
+  {
+    title: "Dragoon Helm",
+    players: [
+      {
+        name: 'dustygriff',
+        wins: 0,
+        losses: 0,
+      },
+      {
+        name: 'leggystarscream',
+        wins: 0,
+        losses: 0,
+      },
+      {
+        name: 'Rexbanner',
+        wins: 0,
+        losses: 0,
+      },
+      {
+        name: 'microKorgs',
+        wins: 0,
+        losses: 0,
+      },
+      {
+        name: 'CommanderLeonhart',
+        wins: 0,
+        losses: 0,
+      },
+      {
+        name: 'ULTROS_PROFESSIONAL',
+        wins: 0,
+        losses: 0,
+      },
+    ]
+  },
+  {
+    title: "Gaea Hat",
+    players: [
+      {
+        name: 'Klopfer',
+        wins: 0,
+        losses: 0,
+      },
+      {
+        name: 'mikemike34',
+        wins: 0,
+        losses: 0,
+      },
+      {
+        name: 'neongrey',
+        wins: 0,
+        losses: 0,
+      },
+      {
+        name: 'SteppeLively',
+        wins: 0,
+        losses: 0,
+      },
+      {
+        name: 'xPankraz',
+        wins: 0,
+        losses: 0,
+      },
+      {
+        name: 'CSRadical',
+        wins: 0,
+        losses: 0,
+      },
+    ]
+  },
+  {
+    title: "Heroine Robe",
+    players: [
+      {
+        name: 'simbu',
+        wins: 0,
+        losses: 0,
+      },
+      {
+        name: 'LadyId19',
+        wins: 0,
+        losses: 0,
+      },
+      {
+        name: 'Rhybon',
+        wins: 0,
+        losses: 0,
+      },
+      {
+        name: 'bgrich419',
+        wins: 0,
+        losses: 0,
+      },
+      {
+        name: 'Eninja81',
+        wins: 0,
+        losses: 0,
+      },
+      {
+        name: 'ZoeVermilion',
+        wins: 0,
+        losses: 0,
+      },
+    ]
+  },
+  {
+    title: "Karate Gi",
+    players: [
+      {
+        name: 'couch_23',
+        wins: 0,
+        losses: 0,
+      },
+      {
+        name: 'A Stone Dingley',
+        wins: 0,
+        losses: 0,
+      },
+      {
+        name: 'Teknolink',
+        wins: 0,
+        losses: 0,
+      },
+      {
+        name: 'FirebirdLover',
+        wins: 0,
+        losses: 0,
+      },
+      {
+        name: 'Guerin',
+        wins: 0,
+        losses: 0,
+      },
+      {
+        name: 'Galeswift',
+        wins: 0,
+        losses: 0,
+      },
+    ]
+  },
+  {
+    title: "Ninja Shirt",
+    players: [
+      {
+        name: 'Arusta',
+        wins: 0,
+        losses: 0,
+      },
+      {
+        name: 'drcossack',
+        wins: 0,
+        losses: 0,
+      },
+      {
+        name: 'Kyrios',
+        wins: 0,
+        losses: 0,
+      },
+      {
+        name: 'ScytheMarshall',
+        wins: 0,
+        losses: 0,
+      },
+      {
+        name: 'DShmoo619',
+        wins: 0,
+        losses: 0,
+      },
+      {
+        name: 'Sheep Launcher',
+        wins: 0,
+        losses: 0,
+      },
+      {
+        name: 'moonblazewolf',
+        wins: 0,
+        losses: 0,
+      },
+    ]
+  },
+  {
+    title: "Zeus Gauntlet",
+    players: [
+      {
+        name: 'LUF',
+        wins: 0,
+        losses: 0,
+      },
+      {
+        name: 'stoppableforce',
+        wins: 0,
+        losses: 0,
+      },
+      {
+        name: 'Just Kinda',
+        wins: 0,
+        losses: 0,
+      },
+      {
+        name: 'Demerine',
+        wins: 0,
+        losses: 0,
+      },
+      {
+        name: 'BoneyC',
+        wins: 0,
+        losses: 0,
+      },
+      {
+        name: 'Beernerd',
+        wins: 0,
+        losses: 0,
+      },
+      {
+        name: 'Poorlydrawnbees',
+        wins: 0,
+        losses: 0,
+      },
+    ]
+  },
+];
+
+// Backup array to save me time, anticipating the list of entrants as a whole list with a group property, I could then assign each one into the proper array
+// const groups = [
+//   {
+//     title: "Artemis Bow",
+//     players: []
+//   },
+//   {
+//     title: "Blitz Whip",
+//     players: []
+//   },
+//   {
+//     title: "Cat Claw",
+//     players: []
+//   },
+//   {
+//     title: "Defense Sword",
+//     players: []
+//   },
+//   {
+//     title: "Full Moon",
+//     players: []
+//   },
+//   {
+//     title: "Lilith Rod",
+//     players: []
+//   },
+//   {
+//     title: "Ogre Axe",
+//     players: []
+//   },
+//   {
+//     title: "Spear Spear",
+//     players: []
+//   },
+//   {
+//     title: "Aegis Shield",
+//     players: []
+//   },
+//   {
+//     title: "Crystal Ring",
+//     players: []
+//   },
+//   {
+//     title: "Dragoon Helm",
+//     players: []
+//   },
+//   {
+//     title: "Gaea Hat",
+//     players: []
+//   },
+//   {
+//     title: "Heroine Robe",
+//     players: []
+//   },
+//   {
+//     title: "Karate Gi",
+//     players: []
+//   },
+//   {
+//     title: "Ninja Shirt",
+//     players: []
+//   },
+//   {
+//     title: "Zeus Gauntlet",
+//     players: []
+//   },
+// ]
