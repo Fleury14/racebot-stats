1c47ab6c2c10e0df9c4ea32b963f76b45fa763ef
J.R. Ruggiero <jrruggiero@shift3tech.com>
2022-03-09 13:28:00 -0800

    chore(matches): initial ac matches data file

---
+++ src/data/ac-matches.js
@@ -0,0 +1,12 @@
+const matches = [
+  {
+    group: 'Defense Sword',
+    racer1: 'Dragus',
+    racer2: 'Gambit017',
+    time1: '1:43:27',
+    time2: '1:55:15',
+    winner: 1
+  }
+]
+
+export default matches;
