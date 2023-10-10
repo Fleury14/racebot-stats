124a02a55347a6a0fc74cd4ea77c8d2e599aadac
J.R. Ruggiero <jrruggiero@bitwiseindustries.com>
2023-10-10 16:48:00 -0700

    chore(parse omg): complete data parser

--- src/components/omg/omg.js
+++ src/components/omg/omg.js
@@ -1,11 +1,27 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Navbar } from '..';
+import { Container, Row, Col } from 'reactstrap';
+import { parseOMG } from '../../helpers/parseOMG';
 
 const OMG = (props) => {
+
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    async function getOMGData() {
+      const OMGdata = await parseOMG();
+      setData(OMGdata);
+    }
+    getOMGData();
+  }, [])
+
+  // const OMGdata = parseOMG();
+  console.log('omgdata', data)
 
   return (
     <div>
       <Navbar />
+      <h1>OMG</h1>
     </div>
   )
 
