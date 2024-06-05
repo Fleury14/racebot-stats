afda35e282df57022977f0e46ca2eec040d705cb
J.R. Ruggiero <jrruggiero@bitwiseindustries.com>
2024-06-05 14:43:58 -0700

    refac(navbar): -ac +zz6

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -29,9 +29,6 @@ const Navbar = (props) => {
           <Link to={'/events'}>
             <li>Events</li>
           </Link>
-          <Link to={'/ac'}>
-            <li>Adamant Cup</li>
-          </Link>
           <Link to={'/zz5'}>
             <li>ZZ5</li>
           </Link>
@@ -40,6 +37,9 @@ const Navbar = (props) => {
           </Link>
           <Link to={'/omg'}>
             <li>OMG</li>
+          </Link>
+          <Link to={'/ZZ6'}>
+            <li>ZZ6</li>
           </Link>
         </ul>
       </div>
